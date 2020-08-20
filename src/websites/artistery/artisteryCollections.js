3a15a3b2e8b8561ddb4a3f7d3ec0f4aa5e5e28e5
TheAsianNextDoor <aaron.scherling11490@gmail.com>
2020-08-20 11:31:18 -0500

    create new page proxies

---
+++ src/websites/artistery/artisteryCollections.js
@@ -0,0 +1,5 @@
+import { navToURL } from '../../utilities/navigationUtils';
+
+export default class ArtisteryCollections {
+    navTo = async () => navToURL('https://theartistrycollection.com/')
+}
---
+++ src/websites/pookster/pooksterCollections.js
@@ -0,0 +1,5 @@
+import { navToURL } from '../../utilities/navigationUtils';
+
+export default class PooksterCollections {
+    navTo = async () => navToURL('https://pookster-palace.myshopify.com/');
+}
