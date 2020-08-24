196518400fa61c4837a4c926935f0e0006dc049d
TheAsianNextDoor <aaron.scherling11490@gmail.com>
2020-08-24 00:33:37 -0500

    Change config to js file

---
+++ src/config.js
@@ -0,0 +1,35 @@
+/**
+ * Config Objects for item to purchase, shipping address, and payment info
+ */
+export const config = {
+    // for lotu purchase
+    user1: {
+        item: '"Peyotero" 3D 2 Piece Hat Pin Sets',
+        email: 'blah@gmail.com',
+        firstName: 'Donald',
+        lastName: 'Trump',
+        address: '123 abc street',
+        city: 'Honesdale',
+        state: 'Pennsylvania',
+        zip: '18431',
+        cardNumber: '1234123412341234',
+        cardNameOn: 'Donald Trump',
+        cardExpirationDate: '01 / 01',
+        cardSecurityCode: '123',
+    },
+    // for artistry purchase
+    user2: {
+        item: 'Ocean Man x "Lion Breath"',
+        email: 'ok@gmail.com',
+        firstName: 'Barrack',
+        lastName: 'Obama',
+        address: '6419 Detroit Avenue',
+        city: 'Cleveland',
+        state: 'Ohio',
+        zip: '44102',
+        cardNumber: '1234123412341234',
+        cardNameOn: 'Barrack Obama',
+        cardExpirationDate: '01 / 01',
+        cardSecurityCode: '123',
+    },
+};
